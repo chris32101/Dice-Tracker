cc8f7e80b6a568731c3038f047fb5ed30397eb70
chris32101 <48632502+chris32101@users.noreply.github.com>
2022-09-28 15:48:22 -0400

    package stuff

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,22 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 import router from './router'
+import store from './store/index.js'
+import Notifications from 'vue-notification'
+import Confetti from 'canvas-confetti'
 
-import './assets/main.css'
+Vue.config.productionTip = false
+Vue.use(Notifications);
+Vue.use(Confetti);
 
-const app = createApp(App)
+if (!localStorage.getItem('loggedIn')) {
+  localStorage.setItem("loggedIn", false);
+  localStorage.setItem("token", "NONE");
+}
 
-app.use(router)
-
-app.mount('#app')
+new Vue({
+  vuetify,
+  store,
+  render: h => h(App),
+}).$mount('#app')
