4843f5afb3b525955e2601872e046b51805e6836
nishi7409 <nishi7409@gmail.com>
2022-10-04 17:40:32 -0400

    update

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>hi</h1>
+  <div class="home">
+    <v-container>
+      <v-row>
+        <div class="hero">
+          <div class="backgroundPicture"></div>
+          <h1 class="headerDiceTracker">Dice Tracker</h1>
+        </div>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style scoped>
+.headerDiceTracker {
+  color: black;
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 25%;
+  transform: translate(-50%, 0%);
+  font-size: 8vh;
+}
+</style>
