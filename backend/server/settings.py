c75230da25f70cc7ae046a0c6fcaa56e89466455
nishi7409 <nishi7409@gmail.com>
2022-10-21 16:22:04 -0400

    update to settings for cors

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -136,3 +136,23 @@ ALLOWED_HOSTS = ['http://localhost:8080', '127.0.0.1', '*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
