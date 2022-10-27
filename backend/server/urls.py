6b8bffd3e96f8a40cd5dc1e8a096ead1a5d5b3ed
chris32101 <48632502+chris32101@users.noreply.github.com>
2022-10-26 23:10:26 -0400

    added some url paths

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
