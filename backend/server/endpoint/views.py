0af053a6c4db7ef0fb68ccc676b8a459b6cfe9ba
chris32101 <48632502+chris32101@users.noreply.github.com>
2022-11-06 21:25:37 -0500

    Update views.py

--- backend/server/endpoint/views.py
+++ backend/server/endpoint/views.py
@@ -27,3 +27,20 @@ class LeagueAddUser(APIView):
                 currentLeague.save()
                 return Response(data={"response": True, "error": "Added user to league object", "leagueName": currentLeague.leagueName})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# Get all active league users (all users that have joined the league)
+class GetActiveLeagueUsers(APIView):
+    def post(self, request, format=None):
+        # serializer checks if the passed in data (json object) meets the desired requirements
+        serializer = LeagueGetActiveUsersSerializer(data=request.data)
+        if serializer.is_valid():
+            #checks for valid league name
+            if (len(League.objects.filter(leagueName=request.data['leagueName'])) == 0):
+                return Response(data={"response": False, "error": "The data for the requested league doesn't exist"})
+            else:
+                #returns all users of the league
+                allUsers = []
+                for x in League.objects.get(leagueName=request.data['leagueName']).allUsers.all():
+                    allUsers.append(x.username)
+                return Response(data={"response": True, "error": allUsers, "leagueOwner": League.objects.get(leagueName=request.data['leagueName']).ownerUsername, "startedStatus": League.objects.get(leagueName=request.data['leagueName']).started, "teamLength": int(League.objects.get(leagueName=request.data['leagueName']).teamLength)})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
